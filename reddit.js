15dfe844c6416930b9900d0f1c3f859766ae6e06
Peyman Aparviz <peyman@dotlabs.co>
2015-11-19 21:25:55 +0100

    Reddit: improve message formatting & cache handling.

--- reddit.js
+++ reddit.js
@@ -16,7 +16,7 @@ module.exports = {
 			name: config.name || "Reddit",
 			icon: config.icon || "https://camo.githubusercontent.com/b13830f5a9baecd3d83ef5cae4d5107d25cdbfbe/68747470733a2f2f662e636c6f75642e6769746875622e636f6d2f6173736574732f3732313033382f313732383830352f35336532613364382d363262352d313165332d383964312d3934376632373062646430332e706e67",
 			channel: config.channel,
-			msgFormat: config.msgFormat || "<%url%|%title%>\r\n%permalink%\r\n_By %author% | Score: %score% | Comments: %num_comments%_",
+			msgFormat: config.msgFormat || "<%url%|%title%>\r\n<https://www.reddit.com%permalink%|_%subreddit%: by %author% | :+1::skin-tone-2: %score% | :speech_balloon: %num_comments% | :mantelpiece_clock: %created_time_str%_>",
 			cronPattern: config.cronPattern,
 			options: {
 				subs: config.subs || "reddit-subs.json",
@@ -36,11 +36,10 @@ module.exports = {
 								ids.push(child.data.id);
 								posts[child.data.id] = child.data;
 							});
-							var cache = storage.getItem(bot.type + "." + cacheKey);
-							var cacheDiff = {added: [], removed: [], common: []};
-							if(cache) {
+							var cache = storage.getItem(bot.type + "." + cacheKey) || [];
+							var cacheDiff = diff(cache, ids);
+							if(cache.length) {
 								//console.log(cacheKey + ":", "cached items found");
-								cacheDiff = diff(cache, ids);
 								if(cacheDiff.added.length) {
 									console.log(cacheKey + ":", "found " + cacheDiff.added.length + " new post(s).");
 									console.log(cacheKey + ":", "added:", cacheDiff.added);
@@ -52,8 +51,7 @@ module.exports = {
 								console.log(cacheKey + ":", "no cache found!");
 								console.log(cacheKey + ":", "current:", ids);
 							}
-							cacheDiff = diff(cache || [], ids);
-							if(cacheDiff.added.length || !cacheDiff.common.length) {
+							if(cacheDiff.added.length) {
 								var cache = cacheDiff.added.concat(cacheDiff.common).concat(cacheDiff.removed);
 								while(cache.length > 100) {
 									cache.pop();
@@ -68,10 +66,13 @@ module.exports = {
 			onPost: function(bot, data, callback) {
 				console.log("posting data:", data.length);
 				data.forEach(function(post) {
+					var createdTimeStr = new Date(post.created_utc * 1000).toString();
+					post.created_time_str = createdTimeStr.slice(0, createdTimeStr.indexOf(' ('));
 					var msgData = {
 						text: bot.msgFormat.replace(/%([^%]+)%/g, function(match, p1) { return post[p1]; }),
 						channel: bot.channel,
-						unfurl_links: true
+						unfurl_media: true,
+						unfurl_links: false
 					};
 					if(bot.icon) {
 						msgData[bot.icon[0] === ":" ? "icon_emoji" : "icon_url"] = bot.icon;
